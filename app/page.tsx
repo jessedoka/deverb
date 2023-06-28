ff786d52ea49824435300302b6eaff3cbeb3dc0a
Jesse <jgodwin1420@gmail.com>
2023-06-28 01:48:34 +0100

    Finished startup - added shadecn

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,35 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8">
-      <h1 className="text-4xl font-bold text-center">
-        PRODFY
-      </h1>
+    <main className="flex flex-col h-screen items-center justify-between p-8">
+      <title>Prodfy</title>
+      <div className='hover:bg-[#000000] hover:text-white block px-3 py-2 rounded-md text-base ease-in-out duration-300'>
+        <h1 className="text-4xl font-bold text-center">
+          Prodfy
+        </h1>
+      </div>
+
+      {/* <div>
+        <Image
+          src="/images/hero.png"
+          alt="Picture of the author"
+          width={100}
+          height={100}
+        />
+      </div> */}
+      
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <h1 className="text-9xl font-bold text-center">One Place.</h1>
+        <h1 className="text-9xl font-bold text-center">Your Production.</h1>
+      </div>
+
+      <div className="flex flex-col items-center justify-center w-full h-full">
+        <div className="flex flex-col items-center justify-center w-full h-full">
+          <h1 className="text-2xl font-bold text-center mb-4">Get notified when we launch</h1>
+          <input className="w-1/2 h-12 px-3 py-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline" type="text" placeholder="Email" />
+          <button className="w-1/2 h-12 px-3 py-2 mt-4 text-base font-semibold text-white transition duration-200 ease-in-out bg-black border border-black rounded-lg hover:bg-white hover:text-black focus:outline-none focus:shadow-outline drop-shadow-xl">Notify Me</button>
+        </div>
+      </div>
     </main>
   )
 }
