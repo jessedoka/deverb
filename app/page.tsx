48e44c08c17aecc82fcdb9e59acffd7bc2927934
Jesse <jgodwin1420@gmail.com>
2023-07-03 14:25:09 +0100

    ablitly to save to db

--- app/page.tsx
+++ app/page.tsx
@@ -8,26 +8,22 @@ export default function Home() {
   const [email, setEmail] = useState('')
   const [loading, setLoading] = useState(false)
 
-  const putEmail = async (e:any) => {
-    e.preventDefault()
-    setLoading(true)
-    const { data, error } = await supabase
-      .from('emails')
-      .insert([{ email }])
-    if (error) {
-      // flash error
-      console.log(error)
-    } else {
-      console.log(data)
+  const handleEmail = async () => {
+    try {
+      setLoading(true)
+      const { error } = await supabase
+        .from('notifications')
+        .insert([{ email }])
+        .single()
+      if (error) throw error
+      alert('You have been added to the list!')
+    } catch (error: any) {
+      alert(error.message)
+    } finally {
+      setLoading(false)
     }
-    setLoading(false)
   }
 
-  const handleEmail = async (e:any) => {
-    e.preventDefault()
-    setEmail(e.target.value)
-    await putEmail(e)
-  }
 
   return (
     <main className="flex flex-col h-screen items-center justify-between p-8">
@@ -56,7 +52,12 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center w-full h-full z-1">
         <div className="flex flex-col items-center justify-center w-full h-full">
           <h1 className="text-2xl font-bold text-center mb-4">Get notified when we launch</h1>
-          <input className="sm:w-1/2 h-12 px-3 py-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline" type="text" placeholder="Email" />
+          <input 
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            className="sm:w-1/2 h-12 px-3 py-2 text-base text-gray-700 placeholder-gray-600 border rounded-lg focus:shadow-outline" placeholder="Email" 
+          />
           <button
           onClick={handleEmail}
           className="w-1/2 h-12 px-6 py-2 mt-4 text-base font-semibold text-white transition duration-200 ease-in-out bg-black rounded-lg shadow-md hover:bg-gray-800 focus:ring-gray-500 focus:ring-offset-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2">Notify Me</button>
