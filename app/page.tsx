b8c9a20c23ed0528a83d6b67e3358484e32f9844
j-doka <jdoka42@gmail.com>
2023-07-13 21:00:37 +0100

    added logo and text logo

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { Database } from "@/lib/database.types"
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useState } from 'react'
 import { ModeToggle } from '@/components/mode-toggle'
+import Image from 'next/image'
 
 export default function Home() {
   const [email, setEmail] = useState('')
@@ -54,9 +55,17 @@ export default function Home() {
         )}
       </div>
       <div className='dark:text-white mt-4 flex gap-4 px-3 py-2 rounded-md text-base ease-in-out duration-300'>
-        <h1 className="text-4xl font-bold text-center">
-          Deverb
-        </h1>
+        {/* dark: deverbl light:deverbd */}
+        {/* show on dark mode */}
+        <div className='dark:hidden'>
+          <Image src='/deverbd.svg' width={200} height={1} alt="deverb logo dark"/>
+        </div>
+
+        {/* show on light mode */}
+        <div className='hidden dark:block'>
+          <Image src='/deverbl.svg' width={200} height={1} alt="deverb logo light"/>
+        </div>
+
         <ModeToggle/>
       </div>
 
