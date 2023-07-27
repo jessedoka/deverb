6b90061a7953991f2a688e0480f92a9601e1d1a5
j-doka <jdoka42@gmail.com>
2023-07-27 20:09:30 +0100

    new avatar style

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -12,7 +12,7 @@ const Avatar = React.forwardRef<
   <AvatarPrimitive.Root
     ref={ref}
     className={cn(
-      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
+      "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full p-1 ring-2 ring-gray-300 dark:ring-gray-500",
       className
     )}
     {...props}
